d5402e7a78bd3690969b67bfd9714d955263dd1c
Nebiyu Samuel <neba.samuel17@gmail.com>
2021-08-14 01:13:14 +0300

    modifies docker file

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '<h1>Welcome!</h1>'
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0", debug=True,port=port)
